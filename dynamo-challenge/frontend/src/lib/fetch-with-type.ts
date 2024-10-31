ca983fd0717ed7d9f522c6d9a058aaf4ced04e94
rhanielmx <rhanielmag.xavier@gmail.com>
2024-10-31 16:17:42 -0300

    putting NX Workspace aside for now

--- dynamo-challenge/frontend/src/lib/fetch-with-type.ts
+++
@@ -1,9 +0,0 @@
-export async function fetchWithType<T>(url: string, options?: RequestInit): Promise<T> {
-    const response = await fetch(url, options);
-    
-    if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    
-    return response.json() as Promise<T>; // Assert the type here
-}
